f84a5207ed91e65a86637c80fbbfb0b12c3ff3b1
Mutuma Gitonga <mgitonga150@gmail.com>
2022-10-28 14:38:58 +0300

    Completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,12 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Mr. Chocolate", 5, "M", "Brown", "mild", 1),
+("Rowdy", 4, "M", "Brown", "mild", 0),
+("Tabitha", 6, "F", "Black", "reserved", 1),
+("Sergeant Brown", 12, "M", "Black", "agile", 1),
+("Melissa", 13, "F", "White", "cool", 1),
+("Grinch", 3, "M", "Brown", "mild", 0),
+("Wendy", 6, "F", "Black", "cool", 0),
+(null, 2, "F", "Black", "mild", 1);
+
+
+
